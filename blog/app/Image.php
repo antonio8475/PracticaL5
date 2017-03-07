92047ced8ff1bff62f131ce4edf1d16510ad9ac5
Antonio Mendoza <antonio8475@gmail.com>
2017-03-07 15:00:00 -0400

    cambio en el campo artcles_id, sigue dando error en la relacion Many to Many

--- blog/app/Image.php
+++ blog/app/Image.php
@@ -8,7 +8,7 @@ class Image extends Model
 {
     protected $table = "images";
 
-    protected $fillable = ['name','article_id'];
+    protected $fillable = ['name','articles_id'];
 
     public function article()
 	    {
