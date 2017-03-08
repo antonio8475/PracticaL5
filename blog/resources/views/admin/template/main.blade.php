10bcd2855042f9cedc8a5afbc3c84ff01a87b159
Antonio Mendoza <antonio8475@gmail.com>
2017-03-08 12:05:10 -0400

    Inicio Practicas de Bootstrap

---
+++ blog/resources/views/admin/template/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        
+        <title></title>
+        <link rel="stylesheet" href="{{ asset('plugins/Bootstrap/css/bootstrap.css')  }}">
+    </head>
+    <body>
+        
+    </body>
+</html>
